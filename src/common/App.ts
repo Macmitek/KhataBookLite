b5707ee9c864d310e969535c5c8fe17c408a2d9e
Mitesh Jadav <jadavmitesh.jd@gmail.com>
2023-05-15 14:19:36 +0530

    Class structure try 1

---
+++ src/common/App.ts
@@ -0,0 +1,46 @@
+import * as express from 'express';
+import { Request, Response } from 'express';
+import { config } from 'dotenv';
+import * as path from 'path';
+import * as bodyParser from 'body-parser';
+
+import customerRouter from './../customer/routes/customerRouter';
+import shopKeeperRouter from './../shopkeeper/routes/shopKeeperRouter';
+
+export class App {
+  private app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.init();
+    this.routes();
+  }
+
+  private init(): void {
+    const ENV_FILE = path.join(__dirname, '..', '.env');
+    config({ path: ENV_FILE });
+
+    this.app.use(express.json({ limit: '50mb' }));
+    this.app.use(express.urlencoded({ limit: '50mb', extended: true }));
+    this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+    this.app.use(bodyParser.json());
+  }
+  
+
+  private routes(): void {
+    this.app.get('/', (req: Request, res: Response) => {
+      res.send('Home Page');
+    });
+
+    this.app.use('/customer', customerRouter);
+    this.app.use('/shopkeeper', shopKeeperRouter);
+  }
+
+  public start(): void {
+    const serverPort = process.env.server_port || 3000;
+
+    this.app.listen(serverPort, () => {
+      console.log('Server listening on port:', serverPort);
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,33 @@
-import bodyParser = require('body-parser');
-import * as express from 'express';
-import customerRouter from './customer/routes/customerRouter';
-import shopKeeperRouter from './shopkeeper/routes/shopKeeperRouter';
-import * as path from 'path';
-import { config } from 'dotenv';
+// import bodyParser = require('body-parser');
+// import * as express from 'express';
+// import customerRouter from './customer/routes/customerRouter';
+// import shopKeeperRouter from './shopkeeper/routes/shopKeeperRouter';
+// import * as path from 'path';
+// import { config } from 'dotenv';
 
 
-const ENV_FILE = path.join(__dirname, '..', '.env');
-config({ path: ENV_FILE });
+// const ENV_FILE = path.join(__dirname, '..', '.env');
+// config({ path: ENV_FILE });
 
 
-const app = express();
+// const app = express();
 
 
-app.use(bodyParser.urlencoded({limit :'50mb', extended : true}));
-app.use(bodyParser.json());
-app.use(bodyParser.json({ limit: '50mb' }));
+// app.use(bodyParser.urlencoded({limit :'50mb', extended : true}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.json({ limit: '50mb' }));
 
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
+// app.get("/", (req, res) => {
+//   res.send("Home Page");
+// });
 
-app.use('/customer', customerRouter);
-app.use('/shopkeeper', shopKeeperRouter);
+// app.use('/customer', customerRouter);
+// app.use('/shopkeeper', shopKeeperRouter);
 
-app.listen(process.env.server_port, () => {
-  console.log("Server listening on port :",process.env.server_port);
-});
+// app.listen(process.env.server_port, () => {
+//   console.log("Server listening on port :",process.env.server_port);
+// });
+import { App } from './common/App';
+
+const app = new App();
+app.start();
